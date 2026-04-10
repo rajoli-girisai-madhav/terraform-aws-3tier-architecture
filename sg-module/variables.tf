b18e1b2bf48cfcbbcb47ff08fbc5dea81ea0fb1a
rajoli-girisai-madhav <rajoligirisai.madhav@gmail.com>
2026-04-10 17:52:50 +0530

    Update variables.tf

--- sg-module/variables.tf
+++ sg-module/variables.tf
@@ -2,18 +2,15 @@ variable "sg_name" {
   description = "Name for the security group"
   type        = string
 }
-
 variable "vpc_id" {
   description = "get VPC ID from vpc module"
   type        = string
 }
-
 variable "tags" {
   description = "A map of tags to assign to the security group"
   type        = map(string)
   default     = {}
 }
-
 variable "ingress_rules" {
   description = "List of ingress rules"
   type = list(object({
